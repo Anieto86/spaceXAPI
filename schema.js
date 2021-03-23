ff3e275bb15f324ffebe5d1643644c34e7c9fc00
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2021-03-24 09:27:00 +1030

    fist commit

---
+++ schema.js
@@ -0,0 +1,65 @@
+const axios = require("axios");
+
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
+} = require("graphql");
+
+//!Launch Type
+const LaunchType = new GraphQLObjectType({
+  name: "Launch",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+  }),
+});
+
+//!Rocket Type
+const RocketType = new GraphQLObjectType({
+  name: "Rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString },
+  }),
+});
+
+//!Root Query para crear endpoints that resolvers our data en este caso de la APi pero pede vernir de nuestra base de datos
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuerryType",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v3/launches")
+          .then((res) => res.data);
+      },
+      },
+      //! creamos un Type especifico para los launch field y que no traiga nada mas 
+      launch: {
+        type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+       return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
